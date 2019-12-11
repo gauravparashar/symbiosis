f2e0ade825ea9c7419826cf03980fb7e6ee21d88
Gaurav Parashar <gauravparashar24@gmail.com>
2019-12-11 15:03:45 +0000

    Added comments to first.py code

--- code/first.py
+++ code/first.py
@@ -1,3 +1,8 @@
+# To execute the code we need to install snakebite python package
+# The aim of the code is to display all the contents of the root of HDFS
+# The client function takes two parameters Hostname or IP address of the NameNode and port number
+# 
+
 from snakebite.client import Client
 
 client=Client('localhost',8020)
